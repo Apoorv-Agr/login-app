3df60ad847fa697e8d6cc5c622c5ae04f160d228
Apoorv <agrawal.av27@gmail.com>
2019-04-27 09:31:35 +0530

    login-app final push

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {Provider} from 'react-redux';
+// import store from './redux/store.js';
+import store from './components/redux/store';
+import LoginForm from './components/LoginForm/LoginForm.js';
+
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <LoginForm />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
